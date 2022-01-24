ea5159c13eb9cc402c77b278b4865d758603f894
Özgür Öney <ozguroneyy@gmail.com>
2022-01-24 23:21:58 +0300

    commit#7
    Clean code approach

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 12 00:33:58 2022
-
+Created on Mon Jan 24 2022 02:33:58 AM
 @author: ozgur oney
 """
 import os
 import random
-import time
-import datetime
     
 from docx import Document
-from docx.opc.coreprops import CoreProperties
 from docx.enum.style import WD_STYLE_TYPE
 from docx.enum.text import WD_ALIGN_PARAGRAPH
-from docx.shared import Inches, Pt
+from docx.shared import Pt
 from tkinter import *
 from tkinter import messagebox
-from tkinter import filedialog
-from tkinter import messagebox
 from tkinter.filedialog import askopenfilename
-from datetime import datetime
 from datetime import date
+
 window = Tk()
 window.title('Workpaper Creator')
-# set minimum window size value
+# set minimum&maximum window size value - for fixed window
 window.minsize(600, 400)
- 
-# set maximum window size value
 window.maxsize(600, 400)
- 
-# window.geometry('400x300')
 window.config(bg='#456')
 f = ('sans-serif', 13)
 btn_font = ('sans-serif', 10)
 bgcolor = '#BF5517'
 
+#array keeping unique title values
 genvar = StringVar()
 genopt = ['Denetçi Yardımcısı', 
           'BT Denetçi Yardımcısı', 
@@ -46,44 +37,50 @@ genopt = ['Denetçi Yardımcısı',
           'Başdenetçi', 
           'Başmüfettiş']
 genvar.set('BT Denetçi Yardımcısı')
-# den_sayisi=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 
+#function of Clear button
 def clear_inputs():
     baslangictarihi.delete(0, 'end')
     bitistarihi.delete(0, 'end')
     denetim_elemani_adi.delete(0, 'end')
     pathLabel.delete(0,'end')
-    
+#change user-input text of date to timestamp object
 def date_to_timestamp(d) :
     day, month, year = d.split('/')
     return date(int(year), int(month), int(day))
-    
+#create random date in given interval
 def randomDate(start, end):
     stime = date_to_timestamp(start)
     etime = date_to_timestamp(end)
     ptime = stime + random.random() * (etime-stime)
     return str(ptime)
-
+#select file contains audit steps
 def select_file():
+    #hide window (optional)
     window.withdraw()
+    #define action
     file_path = askopenfilename(title="Open file", 
                                        filetypes=[("Word",".docx"),("TXT",".txt"),
                                                   ("All files",".*")])
+    #if selected path is valid
     if file_path != "":
         print ("you chose file with path:", file_path)
 
     else:
         print ("you didn't open anything!")
+    #add selected path to label, so that user can see it
     pathLabel.delete(0, END)
     pathLabel.insert(0, file_path)
+    #mutate path so that its valid in all OS'
     file_path = os.path.dirname(file_path)
+    #bring back the lights! you can see window after selection
     window.deiconify()
     return file_path
 
 def generate():
-    # return None
+    #get file path from label
     file_path = pathLabel.get()
-    print(file_path)
+    
     with open (file_path, encoding='utf8') as f:
         # read lines for auto-numbering
         lines = f.readlines()
@@ -92,69 +89,65 @@ def generate():
         for line in lines:
             #create document
             document = Document()
-            
             # Create a character level style Object ("CommentsStyle") then defines its parameters
             obj_styles = document.styles
             obj_charstyle = obj_styles.add_style('CommentStyle', WD_STYLE_TYPE.CHARACTER)
             obj_font = obj_charstyle.font
             obj_font.size = Pt(12)
             obj_font.name = 'Times New Roman'
-            
+            #add header in bold
             baslik = document.add_paragraph()
             baslik.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
-            baslik_run = baslik.add_run('S-100 ÇALIŞMA KAĞIDI', style='CommentStyle').bold=True
-            
+            baslik.add_run('S-100 ÇALIŞMA KAĞIDI', style='CommentStyle').bold=True
+            #create a random date, add it to document
             tarih = document.add_paragraph()
             tarih.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
-            # u_tarih = input("Tarih giriniz: ")
-            tarih_run = tarih.add_run('Tarih: ', style='CommentStyle').bold=True
-            u_tarih = tarih.add_run(randomDate(baslangictarihi.get(), bitistarihi.get()), style='CommentStyle')
-            
+            tarih.add_run('Tarih: ', style='CommentStyle').bold=True
+            tarih.add_run(randomDate(baslangictarihi.get(), bitistarihi.get()), style='CommentStyle')
+            #add workpaper number in increasing order for each step
             ck_num= document.add_paragraph()
             ck_num.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             ck_num.add_run('Çalışma Kâğıdı Numarası: ', style='CommentStyle').bold=True
             index = [x for x in range(len(lines)) if line in lines[x]]
             ck_num.add_run(str(index[0]+1).strip("[]"), style='CommentStyle')
-            
+            #add audit step
             denetim_adimi = document.add_paragraph()
             denetim_adimi.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             denetim_adimi.add_run('İlgili Denetim Adımı: ', style='CommentStyle').bold=True
             denetim_adimi.add_run(line, style='CommentStyle')
-            
+            #add auditor name /with his/her title
             denetim_elemani = document.add_paragraph()
             denetim_elemani.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             denetim_elemani.add_run('Testi Gerçekleştiren Denetim Elemanı:  ', style='CommentStyle').bold=True
-            
-            isim = str(denetim_elemani_adi.get()) +  ", " +  str(genvar.get()) + ", Denetim Genel Müdürlüğü, "
+            isim = str(denetim_elemani_adi.get()) +  ", " +  str(genvar.get()) + ", Denetim Genel Müdürlüğü"
             denetim_elemani.add_run(isim, style='CommentStyle')
-            # print(gui.getAuditorName())
-            # denetim_elemani.add_run(gui.getAuditorName(), style='CommentStyle')
-            
+            #sampling 
             orneklem = document.add_paragraph()
             orneklem.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             orneklem.add_run('Örneklem Yöntemi:  ', style='CommentStyle').bold=True
-            
+            #documents viewed/researched
             incelenen = document.add_paragraph()
             incelenen.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             incelenen.add_run('İncelenen Dokümanlar:  ', style='CommentStyle').bold=True
-        
+            #related (if exists) non-conformity number
             bulgu_num = document.add_paragraph()
             bulgu_num.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             bulgu_num.add_run('Bulgu Numarası:  ', style='CommentStyle').bold=True
-        
+            #test procedure
             test_prod = document.add_paragraph()
             test_prod.paragraph_format.alignment = WD_ALIGN_PARAGRAPH.LEFT
             test_prod.add_run('Ayrıntılı Test Prosedürü:  ', style='CommentStyle').bold=True
-        
+            #detect filename
             filename = line.rstrip() + '.docx'
+            #save document
             document.save(filename)     
+    #message informing last user about process
     messagebox.showinfo("Sonuc", "Dosya(lar) başarıyla oluşturuldu.")
+    #void
     return None
-
-# frames
+# frame carrying items in the window
 frame = Frame(window, padx=20, pady=20, bg=bgcolor)
 frame.pack(expand=True, fill=BOTH)
-
 #labels
 Label(frame, text= "Den. Programı: ", font=f, 
       bg=bgcolor).grid(column=0, row=0, padx=15, pady=15)
@@ -176,7 +169,6 @@ Label(frame, text = "Title: ", font=f,
       bg=bgcolor).grid(column=0, row=4, padx=15, pady=15)
 btn_frame = Frame(frame, bg=bgcolor)
 btn_frame.grid(columnspan=2, pady=(50, 0))
-
 #input widgets   
 fileSelect = Button(
     frame,
@@ -202,11 +194,12 @@ rutbe = OptionMenu(
 rutbe.grid(row=4, column=1, pady=(5,0))
 rutbe.config(width=16, font=f)
 
-#defaults
-baslangictarihi.insert(0,'01/01/2000')
-bitistarihi.insert(0,'14/02/2003')
+#default values
+baslangictarihi.insert(0,'01/01/2022')
+bitistarihi.insert(0,'14/04/2022')
 denetim_elemani_adi.insert(0,'John Doe')
 
+#buttons
 submit_btn = Button(
     btn_frame,
     text='Oluştur',
